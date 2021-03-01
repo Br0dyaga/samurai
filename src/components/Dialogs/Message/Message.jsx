de012dd8089cf61748700ed8b79669e26b562142
EAI <anton.elmanov@gmail.com>
2021-03-01 16:42:55 +0600

    41.1

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -2,21 +2,13 @@ import React from 'react';
 import s from './Message.module.css';
 
 const Message = (props) => {
-	// eslint-disable-next-line default-case
+	let style = '';
 	switch (props.type) {
-		case 'From':
-			return (
-				<div className={s.message + ' ' + s.From}>
-					{props.message}
-				</div>
-			);
-		case 'To':
-			return (
-				<div className={s.message + ' ' + s.To}>
-					{props.message}
-				</div>
-			);
+		case 'From': style = s.message + ' ' + s.From; break;
+		case 'To': style = s.message + ' ' + s.To; break;
+		default: break;
 	}
+	return <div className={style}>{props.message}</div>
 }
 
 export default Message;
